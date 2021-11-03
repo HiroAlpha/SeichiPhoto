3d46154fbb90596cf0a8b9ae831c97fd885e0c0a
Hiro_Alpha <hmkalpha0120@gmail.com>
2021-11-04 01:15:58 +0900

    camera2からcameraxに変更

--- app/src/main/java/com/hiro_alpha/seichiphoto/CameraActivity.java
+++ app/src/main/java/com/hiro_alpha/seichiphoto/CameraActivity.java
@@ -12,6 +12,7 @@ import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
@@ -20,129 +21,86 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextureView cameraPreview;
-    CameraDevice cameraDevice = null;
-    CameraCaptureSession cameraCaptureSession = null;
-    CaptureRequest captureRequest = null;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
+
+    private PreviewView previewView;
+
+    private Camera camera = null;
+    private Preview preview = null;
+    private ImageAnalysis imageAnalysis = null;
+    private ExecutorService cameraExecutor = Executors.newSingleThreadExecutor();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        cameraPreview = findViewById(R.id.cameraPreview);
+        previewView = findViewById(R.id.previewView);
 
-        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
-        //カメラID取得
-        String cameraId = null;
-        try {
-            String[] cameraIdList = cameraManager.getCameraIdList();
-            for (String id : cameraIdList) {
-                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(id);
-
-                switch (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING)) {
-                    //インカメ
-                    case CameraCharacteristics.LENS_FACING_FRONT:
-                        break;
-
-                    //外カメ
-                    case CameraCharacteristics.LENS_FACING_BACK:
-                        cameraId = id;
-                        break;
-                    default:
-                }
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
+        //権限確認
+        if (checkPermissions()){
+            startCamera();
+        } else {
+            Toast.makeText(getApplicationContext() , getString(R.string.error_cameraPermission), Toast.LENGTH_LONG).show();
+            finish();
         }
-
-        //権限確認&カメラオープン
-        try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getApplicationContext() , getString(R.string.error_cameraPermission), Toast.LENGTH_LONG).show();
-                finish();
-            } else {
-                //カメラオープン
-                cameraManager.openCamera(cameraId, stateCallback, null);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-
     }
 
-    //CameraDevice.StateCallback
-    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(@NonNull CameraDevice mCameraDevice) {
-            //接続成功
-            cameraDevice = mCameraDevice;
-            createCameraPreviewSession();
-        }
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        Context context = this;
 
-        @Override
-        public void onDisconnected(@NonNull CameraDevice mCameraDevice) {
-            //接続切断
-            mCameraDevice.close();
-            cameraDevice = null;
-        }
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    preview = new Preview.Builder().build();
+                    CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
 
-        @Override
-        public void onError(@NonNull CameraDevice mCameraDevice, int i) {
-            //エラー
-            mCameraDevice.close();
-            cameraDevice = null;
-        }
-    };
-
-    //カメラプレビュー
-    private void createCameraPreviewSession(){
-        try {
-            SurfaceTexture surfaceTexture = cameraPreview.getSurfaceTexture();
-            surfaceTexture.setDefaultBufferSize(1080, 1920);
-
-            Surface surface = new Surface(surfaceTexture);
-
-            CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            builder.addTarget(surface);
-
-            cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession mCameraCaptureSession) {
-                    //準備完了
-                    cameraCaptureSession = mCameraCaptureSession;
-                    try {
-                        captureRequest = builder.build();
-                        cameraCaptureSession.setRepeatingRequest(captureRequest, null, null);
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
+                    cameraProvider.unbindAll();
+                    camera = cameraProvider.bindToLifecycle((LifecycleOwner) context, cameraSelector, preview);
+                    preview.setSurfaceProvider(previewView.createSurfaceProvider(camera.getCameraInfo()));
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    Toast.makeText(getApplicationContext() , getString(R.string.error_cameraSession), Toast.LENGTH_LONG).show();
-                }
-            }, null);
-
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+            }
+        }, ContextCompat.getMainExecutor(this));
     }
 
     @Override
     public void onClick(View view) {
 
+    }
+
+    //権限確認
+    private boolean checkPermissions(){
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
     }
 }
